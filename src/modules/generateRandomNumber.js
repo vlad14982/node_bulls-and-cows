5866fb0675dd5b597c908148270f2193eeeafc9e
Vlad Hriaznov <vlad14wery@gmail.com>
2025-05-14 23:32:19 -0700

    solution

--- src/modules/generateRandomNumber.js
+++ src/modules/generateRandomNumber.js
@@ -7,7 +7,17 @@
  * @return {number} A random 4-digit number
  */
 function generateRandomNumber() {
-  /* Write your code here */
+  let number = 0;
+
+  while (true) {
+    number = Math.floor(Math.random() * (9876 - 1023 + 1)) + 1023;
+
+    const digits = new Set(number.toString().split(''));
+
+    if (digits.size === 4) {
+      return number;
+    }
+  }
 }
 
 module.exports = {
